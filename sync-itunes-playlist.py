c4932761f918d19a7901f33805a6d3494a0608f5
Thomas Karmann <thomas@krmnn.de>
2012-07-03 11:08:33 +0200

    - made it compatible with python 2.6.1 (osx snow leopard) - added option/argparsing - error handling (empty playlists...) - copy only files that do not already exist

--- sync-itunes-playlist.py
+++ sync-itunes-playlist.py
@@ -1,4 +1,4 @@
-#!/opt/local/bin/python
+#!/usr/bin/python
 
 import optparse
 import sys
@@ -19,55 +19,60 @@ destination="/Volumes/Beta/test/"
 rcmd="rsync --human-readable --size-only -progress"
 
 if __name__ == "__main__":
-    parser = optparse.OptionParser()
+    usage = "usage: %prog [options] <playlist> <destination>"
+    parser = optparse.OptionParser(usage)
     parser.add_option("-l", '--library-file', dest='library_file', 
                       default=os.path.expanduser('~/Music/iTunes/iTunes Library.xml'),
                       help='Location of iTunes library xml file')
-    parser.add_option("--test", action='store_true', dest='test',
-                      default=False, help='Run parser against test.xml')
     options, args = parser.parse_args()
 
-    if options.test:
-        file_loc = 'test.xml'
-    else:
+    if os.path.isfile(options.library_file):
         file_loc = options.library_file
-   
+    else:
+        print "Could not find iTunes Library XML file '%s'!" % options.library_file
+        parser.print_help()
+        sys.exit(False)
+
+    if len(args) != 2:
+        parser.print_help() 
+        sys.exit(1)
+
+    playlist = args[0]
+    destination = args[1]
+
+    if os.path.isdir(destination) == False:
+        print "Destination \'%s\' doesn't exist!" % destination
+        sys.exit(False)
+
     print "Loading iTunes Library... ", 
     f = open(file_loc, 'r')
     handler = SimpleDataHandler()
-    
     xml.sax.parse(f, handler)
-
     final_item = handler.final_item
-
     library = final_item
-    #num_tracks = len(library['Tracks'])
-    #num_playlists = len(library['Playlists'])
-    
-    print "done."
-    #print "Library Summary"
-    #print "%i tracks and %i playlists" % (num_tracks, num_playlists)
+    num_tracks = len(library['Tracks'])
+    num_playlists = len(library['Playlists'])
+    print "done. (%i tracks and %i playlists)" % (num_tracks, num_playlists)
 
     trackids=[]
     for item in library['Playlists']:
         if item['Name'] == playlist:
-            tracks = item['Playlist Items']
-            for elem in tracks:
-                trackids.append(elem['Track ID'])
-
-    print os.path.supports_unicode_filenames
-    print sys.getfilesystemencoding()
+            if 'Playlist Items' in item:
+                tracks = item['Playlist Items']
+                for elem in tracks:
+                    trackids.append(elem['Track ID'])
+            else: 
+                print "Playlist '%s' empty or non-existent!" % playlist
+                sys.exit(False)
 
     for item in trackids:
         cur = library['Tracks'][item]
-        print "Syncing ", cur['Artist'], "-", cur['Name']
+        print "Track: '" + cur['Artist'] + " - " + cur['Name'] + "'...",
         url = cur['Location']
         (scheme, netloc, path, ignored, ignored, ignored)  = urlparse(url)
-        print scheme, netloc, path
         if scheme == "file" and netloc == "localhost":
             pathstr = path.encode('ascii')
             pathstr = urllib2.unquote(pathstr)
-            #print "os.path.isfile:", os.path.isfile(pathstr)
 
             pathelem = pathstr.split('/')
 
@@ -76,16 +81,17 @@ if __name__ == "__main__":
                 os.makedirs(destpath)
 
             srcstr = "\"" + pathstr + "\"" 
+
+            dest = destpath + "/" + pathelem[-1]
             deststr = "\"" + destpath + "/" + pathelem[-1] + "\""
 
+            if os.path.isfile(dest):
+                print "skipped (already exists)"
+                continue
+
             newcmd = rcmd + " " + srcstr + " " + deststr
-            print newcmd
             ret = subprocess.Popen(newcmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            print ret.stdout.read()
-            print ret.stderr.read()
+            print "copied." 
         else:
-            print "skipping: not a local file!"
-
-
-        print "done."
+            print "skipped (not a local file)"
 
